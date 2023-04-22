7bef36a34ac583b07d07787c3589c61df9c324a0
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-04-22 19:22:38 -0300

    Update StateSleep.java

--- states/StateSleep.java
+++ states/StateSleep.java
@@ -1,15 +1,33 @@
 package states;
 import main.Agent;
+import main.Troll;
+import main.Locations;
+
 
 
 public class StateSleep extends State{
 
-    public StateSleep(){
-        super.nameState = "dormindo";
-    }
 
     @Override
     public void execute(Agent agent){
-        System.out.println( "Troll is sleeping in the cave... \n");
+        Troll tr = (Troll) agent; // Downcasting
+
+        if(tr.life <10)
+            ++tr.life;
+
+        if(tr.hunger > 4){
+            tr.currentLocation = Locations.Forest;
+            tr.changeState( new StateEating() );
+        }
+
+        System.out.println( "\n======> Troll is sleeping in the cave... \n");
     }
+    
+/* 
+    @Override
+    public void enter(Agent agent){ }
+
+    @Override
+    public void exit(Agent agent){ }
+*/
 }
