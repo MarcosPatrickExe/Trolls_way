22f2c5cd644e69a27339824d026c576f686d79a6
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-04-22 19:23:51 -0300

    Update StateDead.java

--- states/StateDead.java
+++ states/StateDead.java
@@ -1,15 +1,26 @@
 package states;
 import main.Agent;
+import main.Troll;
+
 
 
 public class StateDead extends State{
 
-    public StateDead(){
-        super.nameState = "morto";
-    }
 
     @Override
     public void execute(Agent agent){
-        System.out.println( "Troll is dead... \n");
+        Troll tro = (Troll) agent; // Downcasting
+
+        String frase = (tro.life == 0) ? "\n ====> The troll is dead! GAME OVER =( \n" : "\n ====> The troll starved to death! GAME OVER =( \n";
+        System.out.println(frase);
     }
+
+/* 
+    @Override
+    public void enter(Agent agent){ }
+
+    @Override
+    public void exit(Agent agent){ }
+*/
+
 }
