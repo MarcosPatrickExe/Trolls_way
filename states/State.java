67708c7c6ffe731e2d916ec8b4bd62b4dc488c30
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-04-22 19:24:14 -0300

    Update State.java

--- states/State.java
+++ states/State.java
@@ -3,8 +3,7 @@ import main.Agent;
 
 
 public abstract class State {
-      public String nameState;
-   
+  
       public abstract void execute(Agent agent);
      // public abstract void enter(Agent agent);
      // public abstract void exit(Agent agent);
