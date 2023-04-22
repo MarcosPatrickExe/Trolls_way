0fb4420a55e2d3a9973257c7b352c0c1e6628c1f
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-04-22 19:23:12 -0300

    Update StateEating.java

--- states/StateEating.java
+++ states/StateEating.java
@@ -1,15 +1,34 @@
 package states;
 import main.Agent;
+import main.Troll;
+import main.Locations;
 
 
 public class StateEating extends State{
     
-    public StateEating(){
-        super.nameState = "comendo";
-    }
 
     @Override
     public void execute(Agent agent){
-        System.out.println( "Troll is eating in the forest... \n");
+        Troll t = (Troll) agent; // Downcasting
+
+        t.hunger -=3;
+        --t.life;
+
+        if(t.hunger <= 4){
+            t.currentLocation = Locations.Cave;
+            t.changeState( new StateSleep() );
+        }
+
+
+        System.out.println( "\n======> Troll is eating in the forest... \n");
     }
+
+    
+/* 
+    @Override
+    public void enter(Agent agent){ }
+
+    @Override
+    public void exit(Agent agent){ }
+*/
 }
