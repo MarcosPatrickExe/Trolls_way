f8406931fd370cf514be6255f8dd73b7fd5418cc
Marcos Patrick <61922327+MarcosPatrickExe@users.noreply.github.com>
2023-04-22 19:20:22 -0300

    Update main.java

--- main/main.java
+++ main/main.java
@@ -2,13 +2,12 @@ package main;
 import java.lang.System;
 import java.util.Scanner;
 import java.lang.Thread;
-
+import java.io.IOException;
+import java.lang.Runtime;
 import states.StateDead;
-import states.StateEating;
+import states.StateFighting;
 import states.StateSleep;
 
-
-enum Locations { Forest, Cave }
 
 
 public class main {
@@ -17,80 +16,67 @@ public class main {
         boolean gameOn = true;
         short cycle = 0;
         Troll troll = new Troll( new StateSleep() );
-        final String separator = "==============================================\n";
+        final String separator = "======================\n";
+        final int threadTime = 2200;
         Scanner scan = new Scanner(System.in);
 
 
         while(gameOn){
-            //System.out.println( "Atualmente o Troll se encontra "+troll.currentState.nameState+"");
-            ++troll.hunger;
-            ++cycle;
+            try{
+                ++troll.hunger;
+                ++cycle;
+
+
+                troll.update();
+                Thread.sleep(threadTime);
+
+
+                if(troll.life <= 0 || troll.hunger >= 10 ){
+                    troll.changeState( new StateDead() );
+                    troll.update();
+                    break;// encerra execucao do 'while'
+                }
+
+                
+                System.out.println( "\n======== ESTADO DO TROLL: ========= \n"+
+                                    "Vida:  "+troll.life+"\n"+
+                                    "Fome:  "+troll.hunger+"\n"+
+                                    "Ciclo atual: "+cycle+"\n"
+                                    + separator
+                );
+                Thread.sleep(threadTime);
 
             
-            if(troll.life == 0 || troll.hunger==10 ){
-                String frase = (troll.life == 0) ? "\n ====> O TROLL MORREU! (Pontos de vida iguais a ZERO) \n" : "\n ====> O TROLL MORREU! (Pontos de fome iguais a 10) \n";
-                System.out.println(frase);
-                troll.changeState( new StateDead() );
-                break;
-            }
+                System.out.println("\n=================== Escolha uma das opces: ====================\n"+
+                                "1- Permanecer no estado atual (ir direto para o proximo turno) \n"+
+                                "2- Aproximar inimigo (inicar batalha) \n"+
+                                "3- Encerrar jogo... \n");
 
+                String option = scan.next();
 
-            if(troll.currentLocation == Locations.Forest){
-                troll.hunger -=3;
-                --troll.life;
-                
-                if(troll.hunger <= 4){
-                    troll.changeState( new StateSleep() );
-                    troll.currentLocation = Locations.Cave;
+            
+                switch(option){
+                    case "1":
+                        Thread.sleep(threadTime);
+                        break;
+                    case "2":
+                        troll.changeState( new StateFighting() );
+                        System.out.print("\033[H\033[2J"); // clearing the console
+                        System.out.flush(); // clearing the console
+                        System.out.println( "\n============ BATALHA INICIADA! =============\n");
+                        Thread.sleep(threadTime);
+                        break;
+                    case "3":
+                        gameOn = false;
+                        break;
+                    default:
+                        System.out.println( "\n ENTRADA DESCONHECIDA! Selecione uma opcao correta! \n");
+                        Thread.sleep(threadTime);
                 }
 
-            }else if(troll.currentLocation == Locations.Cave){
-                if(troll.life <10)
-                    ++troll.life;
-
-                if(troll.hunger > 4){
-                    troll.currentLocation = Locations.Forest;
-                    troll.changeState( new StateEating() );
-                }
+            }catch( InterruptedException ioe){ // catch a Thread and Runtime exceptions
+                System.out.println( "\n Houve um erro durante a execucao da Thread..... \n");
             }
-
-
-        
-            System.out.println( "\n \n============ ESTADO DO TROLL: =========== \n"+
-                                "Vida:  "+troll.life+"\n"+
-                                "Fome:  "+troll.hunger+"\n"+
-                                "Ciclo atual: "+cycle+"\n"
-                                 + separator
-            );
-
-            troll.update();
-          
-            System.out.println("\n=====> Escolha uma das opces: \n"+
-                               "1- Permanecer no estado atual (ir direto para o proximo turno) \n"+
-                               "2- Aproximar inimigo (inicar batalha) \n"+
-                               "3- Encerrar jogo... \n");
-
-            String option = scan.next();
-
-          
-            switch(option){
-                case "1":
-                    break;
-                case "2":
-                    troll.changeState( new StateEating() );
-                    break;
-                case "3":
-                    gameOn = false;
-                    break;
-                default:
-                    System.out.println( "\n ENTRADA DESCONHECIDA! Selecione uma opcao correta! \n");
-                    try{
-                        Thread.sleep(1000);
-                    }catch(Exception ex){
-                        System.out.println("Houve um erro de thread !!");
-                    }
-            }
-          
         }//while
 
         System.exit(0);
@@ -106,8 +92,27 @@ public class main {
 // COMANDO PARA EXECUTAR A APLICACAO  (NO CASO O ARQUIVO .class QUE EH PRINCIAL):
 // java -cp out main.main
 
-
 // java -classpath <directory> <package-name>.<main-file-.class>
+
+
+
+
+
+// TRANSFORMAR ARQUIVO .class EM JAR
+// jar cfe <jar-name>.jar  ./path/to_main  ./path/to/<file-with-main-method>.class
+// jar -cvfm my.jar com/test/myclass/manifest.txt com/test/myclass/MyClass.class
+// jar cfe test.jar out.main.main ./out/main/main.class
+
+
+// jar cfe test.jar ./out/main/main.class ./out/main/main.class
+// java -jar test.jar
+
+
+
+
+// EXECUTAR O JAR:
+// java -jar <jar-name>.jar
+
 
 
 
